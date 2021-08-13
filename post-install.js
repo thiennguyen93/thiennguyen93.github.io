81977da8a25e75a8c1c6896a2565049aacc025bd
thiennguyen93 <phuocthien@outlook.com>
2021-08-13 23:32:30 +0700

    Update post-install.js

--- post-install.js
+++ post-install.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { exec } = require('child_process');
 // npm install git+https://${GH_TOKEN}@github.com/thiennguyen93/${GH_PRIVATE_THEME_REPOSITORY}
-const postInstallCustomTheme = `npm install git+https://${process.env.GH_TOKEN}@github.com/thiennguyen93/${process.env.GH_PRIVATE_THEME_REPOSITORY} --no-save`;
+const postInstallCustomTheme = `npm install git+https://${process.env.GH_TOKEN}@github.com/thiennguyen93/${process.env.GH_PRIVATE_THEME_REPOSITORY}`;
 console.log("run this:" + postInstallCustomTheme);
 console.log("GH_TOKEN:" + process.env.GH_TOKEN);
 exec(postInstallCustomTheme, function(err, stdout, stderr) {

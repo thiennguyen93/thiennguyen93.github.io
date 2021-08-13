69208df3c9a5bce73d369dfda26b60297b6df6a2
thiennguyen93 <phuocthien@outlook.com>
2021-08-13 23:04:45 +0700

    test on netlify

--- post-install.js
+++ post-install.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
-var exec = require('child_process').exec;
+const { exec } = require('child_process');
 // npm install git+https://${GH_TOKEN}@github.com/thiennguyen93/${GH_PRIVATE_THEME_REPOSITORY}
-const postInstallCustomTheme = `npm install git+https://${process.env.GH_TOKEN}@github.com/thiennguyen93/${process.env.GH_PRIVATE_THEME_REPOSITORY}`;
-var cmd = exec(postInstallCustomTheme, function(err, stdout, stderr) {
+const postInstallCustomTheme = `npm install git+https://${process.env.GH_TOKEN}@github.com/thiennguyen93/${process.env.GH_PRIVATE_THEME_REPOSITORY} --no-save`;
+exec(postInstallCustomTheme, function(err, stdout, stderr) {
     if (err) {
       // handle error
+      console.log(stderr)
+    //   exec("npm install git+https://${GH_TOKEN}@github.com/thiennguyen93/${GH_PRIVATE_THEME_REPOSITORY}", function(err1, stdout1, stderr1 ){
+    //       if (err1) {
+    //           console.log('Handle error on netlify');
+    //       }
+    //       console.log('success');
+    //   })
     }
     console.log(stdout);
   });

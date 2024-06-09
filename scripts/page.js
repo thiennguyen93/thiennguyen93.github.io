82806b38bccee6de94455995ab5317a691ab3e3e
Thiện Nguyễn <thien@pilotpulse.ai>
2024-06-10 00:17:50 +0700

    Enabled widgets for category Music, change masonry to 2 columns

--- scripts/page.js
+++ scripts/page.js
@@ -89,9 +89,9 @@ hexo.extend.injector.register(
             trueOrder: false,
             waitForImages: false,
             margin: 0,
-            columns: 3,
+            columns: 2,
             breakAt: {
-                1200: 3,
+                1200: 2,
                 940: 2,
                 520: 1,
                 400: 1

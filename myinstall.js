071f93bd007168030c23d1e66dd6a69477ef761f
Thien Nguyen <hi@thiennguyen.dev>
2024-07-30 22:50:50 +0700

    Update myinstall scripts and icarus config to /cv relative url

--- myinstall.js
+++ myinstall.js
@@ -1,2 +1,9 @@
 const { exec } = require('child_process');
-exec('npm install dotenv').on('exit', () => exec('npm run mypostinstall'));
+exec('npm install dotenv').on('exit', () => exec('npm run mypostinstall', function(err, stdout, stderr) {
+    if (err) {
+      console.log("Da co loi xay ra!!!");
+    }
+    console.log("Custom theme installed successfully")
+    console.log(stdout);
+  })  
+);
--- mypostinstall.js
+++ mypostinstall.js
@@ -1,11 +1,12 @@
-require('dotenv').config();
-const { exec } = require('child_process');
+require("dotenv").config();
+const { exec } = require("child_process");
 const postInstallCustomTheme = `npm install git+https://${process.env.GH_TOKEN}@github.com/thiennguyen93/${process.env.GH_PRIVATE_THEME_REPOSITORY} hexo-tag-aplayer@git+https://github.com/thiennguyen93/thiennguyen-hexo-tag-aplayer --no-save`;
-exec(postInstallCustomTheme, function(err, stdout, stderr) {
-    if (err) {
-      console.log("Da co loi xay ra!!!");
-    }
-    console.log("Custom theme installed successfully")
-    console.log(stdout);
-  });
-  
+exec(postInstallCustomTheme, function (err, stdout, stderr) {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log("Custom theme installed successfully");
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});
